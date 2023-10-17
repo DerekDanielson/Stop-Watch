cb6f89ff44cd44974896952dd2c2900b63f1f905
Derek <derekdanielson84@gmail.com>
2023-10-17 09:31:18 -0600

    Linked HTML elements

---
+++ app.js
@@ -0,0 +1,5 @@
+const timerEl = document.getElementById('timer');
+const startEl = document.getElementById('start');
+const stopEl = document.getElementById('stop');
+const resetEl = document.getElementById('reset');
+
