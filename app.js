e997985badca995dcda365434b932d63b5ac958b
Derek <derekdanielson84@gmail.com>
2023-10-17 09:41:13 -0600

    Added variables

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const stopEl = document.getElementById('stop');
 const resetEl = document.getElementById('reset');
 
 
+let startTime = 0;
+let elapsedTime = 0;
+let timeInterval;
+
 startEl.addEventListener('click', startTimer);
 stopEl.addEventListener('click', stopTimer);
 resetEl.addEventListener('click', resetTimer);
