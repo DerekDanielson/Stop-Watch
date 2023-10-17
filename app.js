881ce28591f3ceca03090e54c05d9af4c7f4ab40
Derek <derekdanielson84@gmail.com>
2023-10-17 10:02:06 -0600

    Added formatTime function and millisecond to display

--- app.js
+++ app.js
@@ -13,7 +13,17 @@ stopEl.addEventListener('click', stopTimer);
 resetEl.addEventListener('click', resetTimer);
 
 function startTimer(){
-    console.log('start');
+    startTime = Date.now() - elapsedTime;
+
+    timeInterval = setInterval(()=>{
+        elapsedTime = Date.now() - startTime;
+        timerEl.textContent = formatTime(elapsedTime);
+    }, 10);
+};
+
+function formatTime(elapsedTime){
+    const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+        return milliseconds > 9 ? milliseconds : '0' + milliseconds;
 };
 
 function stopTimer(){
