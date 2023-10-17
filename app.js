4f0dc8cff2da5cb800730da134c4a07e8fbc0643
Derek <derekdanielson84@gmail.com>
2023-10-17 09:37:01 -0600

    Added event listeners

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ const startEl = document.getElementById('start');
 const stopEl = document.getElementById('stop');
 const resetEl = document.getElementById('reset');
 
+
+startEl.addEventListener('click', startTimer);
+stopEl.addEventListener('click', stopTimer);
+resetEl.addEventListener('click', resetTimer);
+
+function startTimer(){
+    console.log('start');
+};
+
+function stopTimer(){
+    console.log('stop');
+};
+
+function resetTimer(){
+    console.log('reset');
+};
