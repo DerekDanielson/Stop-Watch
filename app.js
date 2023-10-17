a7df88e8cca6f11d31d0234d3e97432da1cce0fb
Derek <derekdanielson84@gmail.com>
2023-10-17 10:22:53 -0600

    Set minutes to display

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ function startTimer(){
 function formatTime(elapsedTime){
     const milliseconds = Math.floor((elapsedTime % 1000) / 10);
     const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
-
+    const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
         return (
+            (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00')
+            + ':' +
             (seconds ? (seconds > 9 ? seconds : '0' + seconds) : '00')
             + '.' +
             (milliseconds > 9 ? milliseconds : '0' + milliseconds));
